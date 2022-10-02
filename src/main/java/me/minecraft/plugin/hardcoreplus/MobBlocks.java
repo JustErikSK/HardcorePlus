e91bffe6039f08b4e64ec33debb334fd7cad0b45
JustErikSK <sebo.erik@gmail.com>
2022-10-02 11:58:40 +0200

    HardcorePlus 1.4, Added 4 more MobBlocks + code rework.

--- src/main/java/me/minecraft/plugin/hardcoreplus/MobBlocks.java
+++ src/main/java/me/minecraft/plugin/hardcoreplus/MobBlocks.java
@@ -1,17 +1,12 @@
 package me.minecraft.plugin.hardcoreplus;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.MagmaCube;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-
 import java.util.Objects;
 import java.util.Random;
 
@@ -23,186 +18,265 @@ public class MobBlocks implements Listener {
         Random random = new Random();
         Player player = event.getPlayer();
         Location loc = player.getLocation();
-        int number15;
+        int number;
+
+        // If player destroys Obsidian, there is a 25% chance for a Phantom to spawn.
         if (block == Material.OBSIDIAN) {
-            number15 = random.nextInt(20);
-            if (number15 >= 16) {
-                this.spawnCreature1(loc);
+            number = random.nextInt(20);
+            if (number >= 16) {
+                this.spawnPhantom(loc);
             }
         }
-        if (block == Material.DRIPSTONE_BLOCK || block == Material.POINTED_DRIPSTONE) {
-            number15 = random.nextInt(20);
-            if (number15 >= 16) {
-                this.spawnCreature2(loc);
+        // If player destroys Dripstone Block or Pointed Dripstone, there is a 25% chance for a Silverfish to spawn.
+        else if (block == Material.DRIPSTONE_BLOCK || block == Material.POINTED_DRIPSTONE) {
+            number = random.nextInt(20);
+            if (number >= 16) {
+                this.spawnSilverfish(loc);
             }
         }
-        if (block == Material.DIORITE) {
-            number15 = random.nextInt(16);
-            if (number15 >= 13) {
-                this.spawnCreature3(loc);
+        // If player destroys Diorite, there is a 25% chance for a Vex to spawn.
+        else if (block == Material.DIORITE) {
+            number = random.nextInt(20);
+            if (number >= 16) {
+                this.spawnVex(loc);
             }
         }
-        if (block == Material.IRON_ORE || block == Material.DEEPSLATE_IRON_ORE) {
-            number15 = random.nextInt(20);
-            if (number15 >= 16) {
-                this.spawnCreature4(loc);
+        // If player destroys Iron Ore or Deepslate Iron Ore, there is a 25% chance for a Drowned Zombie to spawn.
+        else if (block == Material.IRON_ORE || block == Material.DEEPSLATE_IRON_ORE) {
+            number = random.nextInt(20);
+            if (number >= 16) {
+                this.spawnDrownedZombie(loc);
             }
         }
-        if (block == Material.ANCIENT_DEBRIS) {
-            number15 = random.nextInt(40);
-            if (number15 >= 35) {
-                this.spawnCreature5(loc);
+        // If player destroys Ancient Debris, there is a 5% chance for a Wither to spawn.
+        else if (block == Material.ANCIENT_DEBRIS) {
+            number = random.nextInt(40);
+            if (number >= 39) {
+                this.spawnWither(loc);
             }
         }
-        if (block == Material.AMETHYST_CLUSTER) {
-            number15 = random.nextInt(8);
-            if (number15 >= 4) {
-                this.spawnCreature6(loc);
+        // If player destroys Amethyst Cluster, there is 62.5% for a Bat to spawn.
+        else if (block == Material.AMETHYST_CLUSTER) {
+            number = random.nextInt(8);
+            if (number >= 4) {
+                this.spawnBat(loc);
             }
         }
-        if (block == Material.BIG_DRIPLEAF) {
-            number15 = random.nextInt(7);
-            if (number15 >= 3) {
-                this.spawnCreature7(loc);
+        // If player destroys Big Dripleaf, there is a ~71.5% chance for a Cave Spider to spawn.
+        else if (block == Material.BIG_DRIPLEAF) {
+            number = random.nextInt(7);
+            if (number >= 3) {
+                this.spawnCaveSpider(loc);
             }
         }
-        if (block == Material.CACTUS) {
-            number15 = random.nextInt(5);
-            if (number15 >= 3) {
-                this.spawnCreature8(loc);
+        // If player destroys Cactus, there is a 40% chance for a Chicken to spawn.
+        else if (block == Material.CACTUS) {
+            number = random.nextInt(5);
+            if (number >= 4) {
+                this.spawnChicken(loc);
             }
         }
-        if (block == Material.ENDER_CHEST) {
-            number15 = random.nextInt(2);
-            if (number15 > 0) {
-                this.spawnCreature9(loc);
+        // If player destroys Ender Chest, there is a 100% chance for an Endermite to spawn.
+        else if (block == Material.ENDER_CHEST) {
+            this.spawnEndermite(loc);
+        }
+        // If player destroys Gold Ore or Deepslate Gold Ore, there is a ~16.5% chance for a Piglin Brute to spawn.
+        else if (block == Material.GOLD_ORE || block == Material.DEEPSLATE_GOLD_ORE) {
+            number = random.nextInt(12);
+            if (number >= 11) {
+                this.spawnPiglinBrute(loc);
             }
         }
-        if (block == Material.GOLD_ORE || block == Material.DEEPSLATE_GOLD_ORE) {
-            number15 = random.nextInt(12);
-            if (number15 >= 11) {
-                this.spawnCreature10(loc);
+        // If player destroys Lily Pad, there is a 75% chance for a Squid to spawn.
+        else if (block == Material.LILY_PAD) {
+            number = random.nextInt(4);
+            if (number >= 2) {
+                this.spawnSquid(loc);
             }
         }
-        if (block == Material.LILY_PAD) {
-            number15 = random.nextInt(4);
-            if (number15 >= 2) {
-                this.spawnCreature11(loc);
+        // If player destroys Nether Brick, Nether Brick Fence, Nether Brick Slab or Nether Brick Stairs, there is an ~18% chance for a Zoglin to spawn.
+        else if (block == Material.NETHER_BRICK || block == Material.NETHER_BRICK_FENCE || block == Material.NETHER_BRICK_SLAB || block == Material.NETHER_BRICK_STAIRS) {
+            number = random.nextInt(11);
+            if (number >= 10) {
+                this.spawnZoglin(loc);
             }
         }
-        if (block == Material.NETHER_BRICK || block == Material.NETHER_BRICK_FENCE || block == Material.NETHER_BRICK_SLAB || block == Material.NETHER_BRICK_STAIRS) {
-            number15 = random.nextInt(11);
-            if (number15 >= 10) {
-                this.spawnCreature12(loc);
+        // If player destroys Shroomlight, there is a 50% chance for a Zombie Horse to spawn.
+        else if (block == Material.SHROOMLIGHT) {
+            number = random.nextInt(6);
+            if (number >= 4) {
+                this.spawnZombieHorse(loc);
             }
         }
-        if (block == Material.SHROOMLIGHT) {
-            number15 = random.nextInt(6);
-            if (number15 >= 4) {
-                this.spawnCreature13(loc);
+        // If player destroys Sea Pickle, there is a 100% chance for a Pufferfish to spawn.
+        else if (block == Material.SEA_PICKLE) {
+            this.spawnPufferfish(loc);
+        }
+        // If player destroys Soul Sand, there is a ~23.5% chance for a Ghast to spawn.
+        else if (block == Material.SOUL_SAND) {
+            number = random.nextInt(17);
+            if (number >= 14) {
+                this.spawnGhast(loc);
             }
         }
-        if (block == Material.SEA_PICKLE) {
-            number15 = random.nextInt(2);
-            if (number15 > 0) {
-                this.spawnCreature14(loc);
+        // If player destroys Poppy, there is a ~23.5% chance for a Magma Cube to spawn.
+        else if (block == Material.POPPY) {
+            number = random.nextInt(17);
+            if (number >= 14) {
+                this.spawnMagmaCube(loc);
             }
         }
-        if (block == Material.SOUL_SAND || block == Material.SOUL_SOIL) {
-            number15 = random.nextInt(17);
-            if (number15 >= 14) {
-                this.spawnCreature15(loc);
+        // If player destroys Pumpkin, there is a ~23.5% chance for a Spider to spawn.
+        else if  (block == Material.PUMPKIN) {
+            number = random.nextInt(17);
+            if (number >= 14) {
+                this.spawnSpider(loc);
             }
         }
-        if (block == Material.POPPY) {
-            int number16 = random.nextInt(17);
-            if (number16 >= 14) {
-                this.spawnCreature16(loc);
+        // If player destroys Sand, there is a ~23.5% chance for a Husk to spawn.
+        else if (block == Material.SAND) {
+            number = random.nextInt(17);
+            if (number >= 14) {
+                this.spawnHusk(loc);
             }
         }
-        if  (block == Material.PUMPKIN) {
-            int number17 = random.nextInt(17);
-            if (number17 >= 14) {
-                this.spawnCreature17(loc);
+        // If player destroys Slime Block, there is a ~23.5% chance for a Slime to spawn.
+        else if (block == Material.SLIME_BLOCK) {
+            number = random.nextInt(17);
+            if (number >= 14) {
+                this.spawnSlime(loc);
             }
         }
-        if (block == Material.SAND) {
-            int number18 = random.nextInt(17);
-            if (number18 >= 14) {
-                this.spawnCreature18(loc);
+        // If player destroys Wither Rose, there is a ~29.5% chance for a Wither Skeleton to spawn.
+        else if (block == Material.WITHER_ROSE) {
+            number = random.nextInt(17);
+            if (number >= 13) {
+                this.spawnWitherSkeleton(loc);
             }
         }
-        if (block == Material.SLIME_BLOCK) {
-            int number19 = random.nextInt(17);
-            if (number19 >= 14) {
-                this.spawnCreature19(loc);
+        // If player destroys Sponge or Wet Sponge, there is a 40% chance for a Guardian to spawn.
+        else if (block == Material.SPONGE || block == Material.WET_SPONGE) {
+            number = random.nextInt(15);
+            if (number >= 10) {
+                this.spawnGuardian(loc);
             }
         }
-        if (block == Material.WITHER_ROSE) {
-            int number20 = random.nextInt(17);
-            if (number20 >= 14) {
-                this.spawnCreature20(loc);
+        // If player destroys Yellow Bed, there is a 20% chance for a Pillager to spawn.
+        else if (block == Material.YELLOW_BED) {
+            number = random.nextInt(20);
+            if (number >= 17) {
+                this.spawnPillager(loc);
+            }
+        }
+        // If player destroys Spore Blossom, there is a ~16.5% chance for a Tropical Fish to spawn.
+        else if (block == Material.SPORE_BLOSSOM) {
+            number = random.nextInt(12);
+            if (number >= 11) {
+                this.spawnTropicalFish(loc);
+            }
+        }
+        // If player destroys Soul Soil, there is a ~28.5% chance for a Blaze to spawn.
+        else if (block == Material.SOUL_SOIL) {
+            number = random.nextInt(14);
+            if (number >= 11) {
+                this.spawnBlaze(loc);
             }
         }
     }
-    private void spawnCreature1(Location loc) {
+    // Phantom spawn.
+    private void spawnPhantom(Location loc) {
         (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.PHANTOM);
     }
-    private void spawnCreature2(Location loc) {
+    // Silverfish spawn.
+    private void spawnSilverfish(Location loc) {
         (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.SILVERFISH);
     }
-    private void spawnCreature3(Location loc) {
+    // Vex spawn.
+    private void spawnVex(Location loc) {
         (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.VEX);
     }
-    private void spawnCreature4(Location loc) {
+    // Drowned Zombie spawn.
+    private void spawnDrownedZombie(Location loc) {
         (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.DROWNED);
     }
-    private void spawnCreature5(Location loc) {
+    // Wither spawn.
+    private void spawnWither(Location loc) {
         (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.WITHER);
     }
-    private void spawnCreature6(Location loc) {
+    // Bat spawn.
+    private void spawnBat(Location loc) {
         (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.BAT);
     }
-    private void spawnCreature7(Location loc) {
+    // Cave Spider spawn.
+    private void spawnCaveSpider(Location loc) {
         (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.CAVE_SPIDER);
     }
-    private void spawnCreature8(Location loc) {
+    // Chicken spawn.
+    private void spawnChicken(Location loc) {
         (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.CHICKEN);
     }
-    private void spawnCreature9(Location loc) {
-        (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.EVOKER);
+    // Evoker spawn.
+    private void spawnEndermite(Location loc) {
+        (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.ENDERMITE);
     }
-    private void spawnCreature10(Location loc) {
+    // Piglin Brute spawn.
+    private void spawnPiglinBrute(Location loc) {
         (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.PIGLIN_BRUTE);
     }
-    private void spawnCreature11(Location loc) {
+    // Squid spawn.
+    private void spawnSquid(Location loc) {
         (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.SQUID);
     }
-    private void spawnCreature12(Location loc) {
+    // Zoglin spawn.
+    private void spawnZoglin(Location loc) {
         (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.ZOGLIN);
     }
-    private void spawnCreature13(Location loc) {
+    // Zombie Horse spawn.
+    private void spawnZombieHorse(Location loc) {
         (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.ZOMBIE_HORSE);
     }
-    private void spawnCreature14(Location loc) {
+    // Guardian spawn.
+    private void spawnGuardian(Location loc) {
         (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.GUARDIAN);
     }
-    private void spawnCreature15(Location loc) {
+    // Ghast spawn.
+    private void spawnGhast(Location loc) {
         (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.GHAST);
     }
-    private void spawnCreature16(Location loc) {
+    // Magma Cube spawn.
+    private void spawnMagmaCube(Location loc) {
         (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.MAGMA_CUBE);
     }
-    private void spawnCreature17(Location loc) {
+    // Spider spawn.
+    private void spawnSpider(Location loc) {
         (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.SPIDER);
     }
-    private void spawnCreature18(Location loc) {
+    // Husk spawn.
+    private void spawnHusk(Location loc) {
         (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.HUSK);
     }
-    private void spawnCreature19(Location loc) {
+    // Slime spawn.
+    private void spawnSlime(Location loc) {
         (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.SLIME);
     }
-    private void spawnCreature20(Location loc) {
+    // Wither Skeleton spawn.
+    private void spawnWitherSkeleton(Location loc) {
         (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.WITHER_SKELETON);
+    }
+    // Pufferfish spawn.
+    private void spawnPufferfish(Location loc) {
+        (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.PUFFERFISH);
+    }
+    // Pillager spawn.
+    private void spawnPillager(Location loc) {
+        (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.PILLAGER);
+    }
+    // Tropical Fish spawn.
+    private void spawnTropicalFish(Location loc) {
+        (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.TROPICAL_FISH);
+    }
+    // Blaze spawn.
+    private void spawnBlaze(Location loc) {
+        (Objects.requireNonNull(loc.getWorld())).spawnEntity(loc, EntityType.BLAZE);
     }
 }
