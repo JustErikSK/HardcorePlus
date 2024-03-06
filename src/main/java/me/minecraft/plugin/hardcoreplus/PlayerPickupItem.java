c2676c9f460ddb85e6706a73c755236d78f5cd61
juste <sebo.erik@gmail.com>
2024-03-06 20:22:15 +0100

    changed PlayerPickupItem class

--- src/main/java/me/minecraft/plugin/hardcoreplus/PlayerPickupItem.java
+++ src/main/java/me/minecraft/plugin/hardcoreplus/PlayerPickupItem.java
@@ -12,63 +12,60 @@ import org.bukkit.event.Listener;
 
 public class PlayerPickupItem implements Listener {
 
-    @EventHandler // Pickup Event
+    @EventHandler
     public void onPlayerPickUp(EntityPickupItemEvent event) {
-        // PlayerPickupItemEvent is now deprecated, so I replaced it with EntityPickupItemEvent where Entity is the Player.
-        // Sound effects for this class have been temporarily removed because it's not supported for entities.
         LivingEntity entity = event.getEntity();
         ItemStack item = event.getItem().getItemStack();
         Material itemType = item.getType();
 
         if (entity instanceof Player) { // Check if the entity is player
             if (itemType == Material.WITHER_SKELETON_SKULL) {
-                // If WITHER SKELETON SKULL is picked up, player will get:
-                // BLINDNESS and WITHER effect for 4 seconds.
+                // If WITHER SKELETON SKULL is picked up, player will get BLINDNESS and WITHER effect for 4 seconds.
                 entity.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 80, 0));
                 entity.addPotionEffect(new PotionEffect(PotionEffectType.WITHER, 80, 1));
-            } else if (itemType == Material.CAKE) {
-                // If CAKE is picked up, player will get:
-                // REGENERATION 1 effect for 2 seconds.
+            }
+            else if (itemType == Material.CAKE) {
+                // If CAKE is picked up, player will get REGENERATION 1 effect for 2 seconds.
                 entity.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 40, 0));
-            } else if (itemType == Material.BEE_NEST) {
-                // If BEE NEST is picked up, player will get:
-                // LEVITATION effect for 1 seconds.
+            }
+            else if (itemType == Material.BEE_NEST) {
+                // If BEE NEST is picked up, player will get LEVITATION effect for 1 second.
                 entity.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 20, 1));
-            } else if (itemType == Material.BELL) {
-                // If BELL is picked up, player will get:
-                // SATURATION effect for 2 seconds.
+            }
+            else if (itemType == Material.BELL) {
+                // If BELL is picked up, player will get SATURATION effect for 2 seconds.
                 entity.addPotionEffect(new PotionEffect(PotionEffectType.SATURATION, 40, 0));
-            } else if (itemType == Material.DRAGON_EGG) {
-                // If DRAGON EGG is picked up, player will get:
-                // REGENERATION 2 effect for 2 seconds.
+            }
+            else if (itemType == Material.DRAGON_EGG) {
+                // If DRAGON EGG is picked up, player will get REGENERATION 2 effect for 2 seconds.
                 entity.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 40, 1));
-            } else if (itemType == Material.DRAGON_HEAD) {
-                // If DRAGON HEAD is picked up, player will get:
-                // LEVITATION effect for 3 seconds.
+            }
+            else if (itemType == Material.DRAGON_HEAD) {
+                // If DRAGON HEAD is picked up, player will get LEVITATION effect for 3 seconds.
                 entity.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 60, 0));
-            } else if (itemType == Material.WITHER_ROSE) {
-                // If WITHER ROSE is picked up, player will get:
-                // WITHER effect for 1 second.
+            }
+            else if (itemType == Material.WITHER_ROSE) {
+                // If WITHER ROSE is picked up, player will get WITHER effect for 1 second.
                 entity.addPotionEffect(new PotionEffect(PotionEffectType.WITHER, 20, 0));
-            } else if (itemType == Material.SHROOMLIGHT) {
-                // If SHROOMLIGHT is picked up, player will get:
-                // GLOWING effect for 3 seconds.
+            }
+            else if (itemType == Material.SHROOMLIGHT) {
+                // If SHROOMLIGHT is picked up, player will get GLOWING effect for 3 seconds.
                 entity.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 60, 0));
-            } else if (itemType == Material.TNT) {
-                // If TNT is picked up, player will get:
-                // instant HARM 2 effect.
+            }
+            else if (itemType == Material.TNT) {
+                // If TNT is picked up, player will get INSTANT HARM 2 effect.
                 entity.addPotionEffect(new PotionEffect(PotionEffectType.HARM, 20,1));
-            } else if (itemType == Material.AXOLOTL_BUCKET) {
-                // If AXOLOTL BUCKET is picked up, player will get:
-                // ABSORPTION effect for 4 seconds.
+            }
+            else if (itemType == Material.AXOLOTL_BUCKET) {
+                // If AXOLOTL BUCKET is picked up, player will get ABSORPTION effect for 4 seconds.
                 entity.addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION, 80, 0));
-            } else if (itemType == Material.BLAZE_ROD) {
-                // If BLAZE ROD is picked up, player will get:
-                // FIRE RESISTANCE effect for 1 second.
+            }
+            else if (itemType == Material.BLAZE_ROD) {
+                // If BLAZE ROD is picked up, player will get FIRE RESISTANCE effect for 1 second.
                 entity.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 20, 0));
-            } else if (itemType == Material.GHAST_TEAR) {
-                // If GHAST TEAR is picked up, player will get:
-                // SLOWNESS 2 effect for 3 seconds.
+            }
+            else if (itemType == Material.GHAST_TEAR) {
+                // If GHAST TEAR is picked up, player will get SLOWNESS 2 effect for 3 seconds.
                 entity.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 60, 1));
             }
         }
