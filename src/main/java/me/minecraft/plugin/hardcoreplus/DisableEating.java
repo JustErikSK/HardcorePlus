defdab43407556732bcc39d76af8e6b2f6799ee9
juste <sebo.erik@gmail.com>
2024-03-13 10:56:48 +0100

    removed bread and golden apple - DisableEating class

--- src/main/java/me/minecraft/plugin/hardcoreplus/DisableEating.java
+++ src/main/java/me/minecraft/plugin/hardcoreplus/DisableEating.java
@@ -11,12 +11,17 @@ public class DisableEating implements Listener {
     @EventHandler
     public void onFoodEating(PlayerItemConsumeEvent event) {
         ItemStack consumed = event.getItem();
-        if (consumed.getType().equals(Material.GOLDEN_APPLE) || consumed.getType().equals(Material.BREAD) ||
-                consumed.getType().equals(Material.COOKED_BEEF) || consumed.getType().equals(Material.COOKED_CHICKEN) ||
-                consumed.getType().equals(Material.COOKED_COD) || consumed.getType().equals(Material.COOKED_MUTTON) ||
-                consumed.getType().equals(Material.COOKED_PORKCHOP) || consumed.getType().equals(Material.COOKED_RABBIT) ||
-                consumed.getType().equals(Material.COOKED_SALMON) || consumed.getType().equals(Material.BAKED_POTATO) ||
-                consumed.getType().equals(Material.GOLDEN_CARROT) || consumed.getType().equals(Material.PUMPKIN_PIE)) {
+        if (consumed.getType().equals(Material.COOKED_BEEF) ||
+            consumed.getType().equals(Material.COOKED_CHICKEN) ||
+            consumed.getType().equals(Material.COOKED_COD) ||
+            consumed.getType().equals(Material.COOKED_MUTTON) ||
+            consumed.getType().equals(Material.COOKED_PORKCHOP) ||
+            consumed.getType().equals(Material.COOKED_RABBIT) ||
+            consumed.getType().equals(Material.COOKED_SALMON) ||
+            consumed.getType().equals(Material.BAKED_POTATO) ||
+            consumed.getType().equals(Material.GOLDEN_CARROT) ||
+            consumed.getType().equals(Material.PUMPKIN_PIE)) {
+
             event.setCancelled(true);
         }
     }
