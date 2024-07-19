ef6f124a0be85bdf76a326b879e1f5456c4cae30
justerik <sebo.erik@gmail.com>
2024-07-19 15:04:48 +0200

    Replaced the old tags

--- src/main/java/me/minecraft/plugin/hardcoreplus/HardcorePlus.java
+++ src/main/java/me/minecraft/plugin/hardcoreplus/HardcorePlus.java
@@ -29,60 +29,56 @@ public final class HardcorePlus extends JavaPlugin {
         this.getServer().getPluginManager().registerEvents(new HarderLoot(), this);
 
         // Scheduler for random effects
-        // Random effects will be reworked
-            getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
-                @Override
-                public void run() {
-                    for (Player player : getServer().getOnlinePlayers()) {
-                        Random random = new Random();
-                        int number = random.nextInt(9);
-                        long time = player.getWorld().getTime();
+            getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
+                for (Player player : getServer().getOnlinePlayers()) {
+                    Random random = new Random();
+                    int number = random.nextInt(9);
+                    long time = player.getWorld().getTime();
 
-                        if (time == 6000) {
-                            if (number == 0) { // 0 = Regeneration
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 1200, 1));
-                            } else if (number == 1) { // 1 = Slow Digging
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 300, 0));
-                            } else if (number == 2) { // 2 = Speed
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1200, 1));
-                            } else if (number == 3) { // 3 = Poison
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 300, 0));
-                            } else if (number == 4) { // 4 = Fire Resistance
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 1200, 1));
-                            } else if (number == 5) { // 5 = Damage Resistance
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 1200, 1));
-                            } else if (number == 6) { // 6 = Weakness
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 300, 0));
-                            } else if (number == 7) { // 7 = Jump Boost
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 1200, 1));
-                            } else { // 8 = Levitation
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 300, 0));
-                            }
+                    if (time == 6000) {
+                        if (number == 0) { // 0 = Regeneration
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 1200, 1));
+                        } else if (number == 1) { // 1 = Slow Digging
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.MINING_FATIGUE, 300, 0));
+                        } else if (number == 2) { // 2 = Speed
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1200, 1));
+                        } else if (number == 3) { // 3 = Poison
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 300, 0));
+                        } else if (number == 4) { // 4 = Fire Resistance
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 1200, 1));
+                        } else if (number == 5) { // 5 = Damage Resistance
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.RESISTANCE, 1200, 1));
+                        } else if (number == 6) { // 6 = Weakness
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 300, 0));
+                        } else if (number == 7) { // 7 = Jump Boost
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP_BOOST, 1200, 1));
+                        } else { // 8 = Levitation
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 300, 0));
                         }
-                        // If game time is at 18000 ticks, one of these midnight effects will be applied to all players.
-                        // 5 effects are good and other 5 are bad.
-                        // Good effects have duration of 360 ticks (18 seconds) and amplifier of 0.
-                        // Bad effects have duration of 600 ticks (30 seconds) and amplifier of 0.
-                        else if (time == 18000) {
-                            if (number == 0) { // 0 = Darkness
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.DARKNESS, 600, 0));
-                            } else if (number == 1) { // 1 = Saturation
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.SATURATION, 360, 0));
-                            } else if (number == 2) { // 2 = Nausea
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 600, 0));
-                            } else if (number == 3) { // 3 = Fast Digging
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, 360, 0));
-                            } else if (number == 4) { // 4 = Wither
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.WITHER, 600, 0));
-                            } else if (number == 5) { // 5 = Health Boost
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST, 360, 0));
-                            } else if (number == 6) { // 6 = Blindness
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 600, 0));
-                            } else if (number == 7) { // 7 = Strength
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 360, 0));
-                            } else { // 8 = Slow Digging
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 600, 0));
-                            }
+                    }
+                    // If game time is at 18000 ticks, one of these midnight effects will be applied to all players.
+                    // 5 effects are good and other 5 are bad.
+                    // Good effects have duration of 360 ticks (18 seconds) and amplifier of 0.
+                    // Bad effects have duration of 600 ticks (30 seconds) and amplifier of 0.
+                    else if (time == 18000) {
+                        if (number == 0) { // 0 = Darkness
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.DARKNESS, 600, 0));
+                        } else if (number == 1) { // 1 = Saturation
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.SATURATION, 360, 0));
+                        } else if (number == 2) { // 2 = Nausea
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.NAUSEA, 600, 0));
+                        } else if (number == 3) { // 3 = Fast Digging
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.HASTE, 360, 0));
+                        } else if (number == 4) { // 4 = Wither
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.WITHER, 600, 0));
+                        } else if (number == 5) { // 5 = Health Boost
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST, 360, 0));
+                        } else if (number == 6) { // 6 = Blindness
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 600, 0));
+                        } else if (number == 7) { // 7 = Strength
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.STRENGTH, 360, 0));
+                        } else { // 8 = Slow Digging
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.MINING_FATIGUE, 600, 0));
                         }
                     }
                 }
